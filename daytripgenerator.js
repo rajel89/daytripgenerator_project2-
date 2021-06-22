6db885632a5d501226d3155a15ef1e04520d956a
rajel89 <rajah.ello@yahoo.com>
2021-06-21 21:48:32 -0500

    entered comments in VC

--- daytripgenerator.js
+++ daytripgenerator.js
@@ -1,2 +1,29 @@
 "use strict";
 
+// (5 points): As a developer, I want to make at least three commits with descriptive messages.
+
+let userName = prompt("Hello! please enter your name")
+console.log("Welcome "+ userName + " the system will now pick a randome location for you" );
+
+
+
+
+
+
+
+// (5 points): As a user, I want a destination to be randomly selected for my day trip.
+
+
+
+
+
+
+
+// (5 points): As a user, I want a restaurant to be randomly selected for my day trip.
+// (5 points): As a user, I want a mode of transportation to be randomly selected for my day trip.
+// (5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.
+// (15 points): As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don’t like one or more of those things.
+// (10 points): As a user, I want to be able to confirm that my day trip is “complete” once I like all of the random selections.
+// (10 points): As a user, I want to display my completed trip in the console.
+// (5 points): As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing!
+
